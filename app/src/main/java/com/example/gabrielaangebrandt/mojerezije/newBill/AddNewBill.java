5167e1ba0b77b8a2c8f1c1218323004151c66f59
lkordic <lkordic@hotmail.com>
2017-11-27 14:22:43 +0100

    vjhggjhgjhg

--- app/src/main/java/com/example/gabrielaangebrandt/mojerezije/newBill/AddNewBill.java
+++ app/src/main/java/com/example/gabrielaangebrandt/mojerezije/newBill/AddNewBill.java
@@ -1,15 +1,13 @@
 package com.example.gabrielaangebrandt.mojerezije.newBill;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -17,14 +15,8 @@ import com.example.gabrielaangebrandt.mojerezije.App;
 import com.example.gabrielaangebrandt.mojerezije.R;
 import com.example.gabrielaangebrandt.mojerezije.listOfBills.ListOfBills;
 import com.example.gabrielaangebrandt.mojerezije.model.data_models.Bill;
-import com.example.gabrielaangebrandt.mojerezije.model.data_models.User;
-//import com.example.gabrielaangebrandt.mojerezije.utils.FirebaseUtils;
 import com.example.gabrielaangebrandt.mojerezije.utils.RealmUtils;
 import com.example.gabrielaangebrandt.mojerezije.utils.SharedPrefs;
-import com.google.android.gms.vision.CameraSource;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//1import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,22 +29,35 @@ import io.realm.RealmList;
 
 import static java.lang.Integer.parseInt;
 
+//import com.example.gabrielaangebrandt.mojerezije.utils.FirebaseUtils;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//1import com.google.firebase.database.ValueEventListener;
+
 public class AddNewBill extends AppCompatActivity {
     @BindView(R.id.et_naziv)
     EditText vrsta;
+
     @BindView(R.id.et_tvrtka)
     EditText tvrtka;
-    @BindView(R.id.et_datumZaprimljenosti)
-    EditText datumDospijeca;
+
+    @BindView(R.id.et_brojRacuna)
+    EditText brojRacuna;
+
     @BindView(R.id.et_mjesec)
     EditText mjesec;
+
     @BindView(R.id.et_iznos)
     EditText iznos;
+
     @BindView(R.id.rb_placen)
     RadioButton rb_placen;
+
     @BindView(R.id.rb_neplacen)
     RadioButton rb_neplacen;
+
     //    DataSnapshot dataSnapshot;
+    String barcodeData;
 
     List<Bill> listOfBills = new ArrayList<>();
     Bill bill;
@@ -75,24 +80,24 @@ public class AddNewBill extends AppCompatActivity {
     public void addBill() {
 //        realm.beginTransaction();
         if (rb_placen.isChecked()) {
-            if (vrsta.getText().toString().equals("") || tvrtka.getText().toString().equals("") || datumDospijeca.getText().toString().equals("") ||
+            if (vrsta.getText().toString().equals("") || tvrtka.getText().toString().equals("") || brojRacuna.getText().toString().equals("") ||
                     mjesec.getText().toString().equals("") || iznos.getText().toString().equals("")) {
                 Toast.makeText(this, R.string.elementsArentEntered, Toast.LENGTH_SHORT).show();
             } else {
 //                User user = FirebaseUtils.checkIfUserExists(username, dataSnapshot);
 //                User user = RealmUtils.checkIfUserExists("username", SharedPrefs.getSharedPrefs("username", this));
-                bill = new Bill(parseInt(mjesec.getText().toString()), datumDospijeca.getText().toString(), vrsta.getText().toString(), tvrtka.getText().toString(), iznos.getText().toString(), "rb_placen");
+                bill = new Bill(parseInt(mjesec.getText().toString()), brojRacuna.getText().toString(), vrsta.getText().toString(), tvrtka.getText().toString(), iznos.getText().toString(), "rb_placen");
                 listOfBills = RealmUtils.getUsersBills("username", SharedPrefs.getSharedPrefs("username", this));
             }
         } else {
-            if (vrsta.getText().toString().equals("") || tvrtka.getText().toString().equals("") || datumDospijeca.getText().toString().equals("") ||
+            if (vrsta.getText().toString().equals("") || tvrtka.getText().toString().equals("") || brojRacuna.getText().toString().equals("") ||
                     mjesec.getText().toString().equals("") || iznos.getText().toString().equals("")) {
                 Toast.makeText(this, R.string.elementsArentEntered, Toast.LENGTH_SHORT).show();
             } else {
 //                User user = RealmUtils.checkIfUserExists("username", SharedPrefs.getSharedPrefs("username", this));
 //               User user = FirebaseUtils.checkIfUserExists(username, dataSnapshot);
 
-                bill = new Bill(parseInt(mjesec.getText().toString()), datumDospijeca.getText().toString(), vrsta.getText().toString(), tvrtka.getText().toString(), iznos.getText().toString(), "rb_placen");
+                bill = new Bill(parseInt(mjesec.getText().toString()), brojRacuna.getText().toString(), vrsta.getText().toString(), tvrtka.getText().toString(), iznos.getText().toString(), "rb_placen");
                 listOfBills = RealmUtils.getUsersBills("username", SharedPrefs.getSharedPrefs("username", this));
 
             }
@@ -129,12 +134,24 @@ public class AddNewBill extends AppCompatActivity {
         startActivityForResult(CameraActivity.getLaunchIntent(this), CAMERA_PIC_REQUEST);
     }
 
+    @SuppressLint("SetTextI18n")
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_PIC_REQUEST && resultCode==RESULT_OK) {
+        if (requestCode == CAMERA_PIC_REQUEST && resultCode == RESULT_OK) {
 
-            String barcodeData = (String) data.getExtras().get(CameraActivity.KEY_EXTRA_DATA);
-            Toast.makeText(this, barcodeData, Toast.LENGTH_SHORT).show();
+            barcodeData = (String) data.getExtras().get(CameraActivity.KEY_EXTRA_DATA);
+            String[] parts = barcodeData.split("\n");
+            Float f =  Float.parseFloat(parts[2]);
+            String ff = Float.toString(f);
+            iznos.setText(new StringBuilder(ff).insert(ff.length()-2, ",").toString());
+            tvrtka.setText(parts[6] + ", " + parts[7] + ", " + parts[8]);
+            vrsta.setText(parts[12]);
+            rb_neplacen.isChecked();
+            brojRacuna.setText(parts[13]);
+            String[] string = parts[11].split("-");
+            mjesec.setText(new StringBuilder(string[1]).insert(string[1].length()-2, "/").toString());
+
         }
-    }
 
+    }
 }
+
